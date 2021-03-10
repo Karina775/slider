90f4809d5af942adf4f4f2ff65889e88e852b60d
Карина775 <saprykinakarina@bk.ru>
2021-03-10 13:28:50 +0300

    creating a slider

--- JavaScript.js
+++ JavaScript.js
@@ -2,6 +2,15 @@ let offset = 0; // смещение от левого края
 const sliderLine= document.querySelector ('.slider-line');
 
 document.querySelector('.slider-next').addEventListener('click', function() {
+    offset = offset - 400;
+    if (offset < 0) {
+        offset = 1200;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
+
+
+document.querySelector('.slider-prev').addEventListener('click', function() {
     offset = offset + 400;
     if (offset > 1200) {
         offset = 0;
@@ -9,10 +18,4 @@ document.querySelector('.slider-next').addEventListener('click', function() {
     sliderLine.style.left = -offset + 'px';
 });
 
-document.querySelector('.slider-prev').addEventListener('click', function() {
-    offset = offset - 400;
-    if (offset < 0) {
-        offset = 1200;
-    }
-    sliderLine.style.left = -offset + 'px';
-});
+
